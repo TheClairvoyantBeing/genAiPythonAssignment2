c655a03b2ee6593478ae0c8904bdb54d2d6f96b8
Evion Cutinha <125786192+13-Clairvoyance@users.noreply.github.com>
2026-02-23 22:15:36 +0530

    finnished assignment: q02, used floating point to cover edge cases also used try-catch in case of devide-by-zero and invalid format errors

--- q02_simple_calculator.py
+++ q02_simple_calculator.py
@@ -16,3 +16,52 @@
 # 10 ^ 3 = 1000
 
 # --- Write your solution below this line ---
+
+try:
+    # Asks user to enter 2 numbers
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("\nResults:")
+
+    # Addition
+    print(f"{num1} + {num2} = {num1 + num2}")
+
+    # Subtraction
+    print(f"{num1} - {num2} = {num1 - num2}")
+
+    # Multiplication
+    print(f"{num1} * {num2} = {num1 * num2}")
+
+    # Division - check for zero before dividing
+    if num2 == 0:
+        print(f"{num1} / {num2} = Cannot divide by zero")
+    else:
+        print(f"{num1} / {num2} = {num1 / num2:.2f}")
+
+    # Modulus - check for zero before using %
+    if num2 == 0:
+        print(f"{num1} % {num2} = Cannot modulus by zero")
+    else:
+        print(f"{num1} % {num2} = {num1 % num2}")
+
+    # Exponentiation
+    print(f"{num1} ^ {num2} = {num1 ** num2}")
+
+except ValueError:
+    print("Error: Please enter valid numbers only.")
+
+
+# OUTPUT:
+
+# PS C:\Users\...\Documents\Genai\Assignment2> python q02_simple_calculator.py
+# Enter first number: 10
+# Enter second number: 3
+
+# Results:
+# 10.0 + 3.0 = 13.0
+# 10.0 - 3.0 = 7.0
+# 10.0 * 3.0 = 30.0
+# 10.0 / 3.0 = 3.33
+# 10.0 % 3.0 = 1.0
+# 10.0 ^ 3.0 = 1000.0
